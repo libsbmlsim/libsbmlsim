c9ed3458f611d2195dafd647011706de3439dae5
Akira Funahashi <funa@bio.keio.ac.jp>
2012-03-14 23:58:43 +0900

    Update version info to 0.9.4

--- src/libsbmlsim/version.h
+++ src/libsbmlsim/version.h
@@ -1,8 +1,8 @@
 #ifndef LibSBMLSim_Version_h
 #define LibSBMLSim_Version_h
 
-#define LIBSBMLSIM_DOTTED_VERSION "0.9.3"
-#define LIBSBMLSIM_VERSION 903
-#define LIBSBMSIML_VERSION_STRING "00903"
+#define LIBSBMLSIM_DOTTED_VERSION "0.9.4"
+#define LIBSBMLSIM_VERSION 904
+#define LIBSBMSIML_VERSION_STRING "00904"
 
 #endif  /* LibSBMLSim_Version_h */
