71e7c112f9233fe24f77c0ce1d738fa4ec363ea2
Akito Tabira <mythosil2008@gmail.com>
2012-03-15 23:40:03 +0900

    Fixed new _myResult(). This should be done AFTER System.loadLibrary()

--- Test.java
+++ Test.java
@@ -4,9 +4,10 @@ import org.sbml.libsbml.libsbml;
 
 public class Test {
   public static void main(String[] args) {
-    _myResult result = new _myResult();
     System.loadLibrary("sbmlsim");
     System.loadLibrary("sbmlj");
+
+    _myResult result = new _myResult();
     SBMLDocument d = libsbml.readSBML("./src/MAPK.xml");
     result = libsbmlsim.simulateSBMLFromString(d.toSBML(), 4000.0, 0.1, 100, 1, 41, 0);
     System.out.println(result.getColumn_name_time());
