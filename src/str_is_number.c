73ffcf280c68924ce28d8934a8f3dd681e42419b
Akito Tabira <mythosil2008@gmail.com>
2012-03-15 12:29:14 +0900

    refactored str_is_number function to use isdigit()

--- src/str_is_number.c
+++ src/str_is_number.c
@@ -1,18 +1,22 @@
 #include "libsbmlsim/libsbmlsim.h"
 
-boolean str_is_number(const char *str){
-  int i = 0;
-  int dot_count = 0;
-  while(str[i] != '\0'){
-    if((str[i] < 48 || str[i] > 57) && str[i] != 46){
-      return 0;
-    }else if(str[i] == 46){
-      dot_count++;
-      if(dot_count > 1){
-        return 0;
-      }
+boolean str_is_number(const char *str)
+{
+  boolean isfloat = false;
+  if (*str == '\0')
+    return false;
+  while (*str != '\0') {
+    if (isdigit(*str)) {
+      ;
+    } else if (*str == '.') {
+      if (isfloat)
+        return false;
+      isfloat = true;
+    } else {
+      return false;
     }
-    i++;
+    str++;
   }
-  return 1;
+  return true;
 }
+
