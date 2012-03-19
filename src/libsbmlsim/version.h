dc74e590716b2533ac45a3e4ab4554db5f83a3d3
Akira Funahashi <funa@bio.keio.ac.jp>
2012-03-20 02:06:16 +0900

    Yeaaaahh! 0.9.5! getting close to our RELEASE!

--- src/libsbmlsim/version.h
+++ src/libsbmlsim/version.h
@@ -1,8 +1,8 @@
 #ifndef LibSBMLSim_Version_h
 #define LibSBMLSim_Version_h
 
-#define LIBSBMLSIM_DOTTED_VERSION "0.9.4"
-#define LIBSBMLSIM_VERSION 904
-#define LIBSBMSIML_VERSION_STRING "00904"
+#define LIBSBMLSIM_DOTTED_VERSION "0.9.5"
+#define LIBSBMLSIM_VERSION 905
+#define LIBSBMSIML_VERSION_STRING "00905"
 
 #endif  /* LibSBMLSim_Version_h */
