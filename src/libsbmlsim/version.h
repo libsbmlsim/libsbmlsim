800d1c2469f4b2954c198dfe565828046a8bde32
Akira Funahashi <funa@bio.keio.ac.jp>
2012-03-20 06:19:35 +0900

    Tacky fix, and Perl binding fix. 0.9.6!

--- src/libsbmlsim/version.h
+++ src/libsbmlsim/version.h
@@ -1,8 +1,8 @@
 #ifndef LibSBMLSim_Version_h
 #define LibSBMLSim_Version_h
 
-#define LIBSBMLSIM_DOTTED_VERSION "0.9.5"
-#define LIBSBMLSIM_VERSION 905
-#define LIBSBMSIML_VERSION_STRING "00905"
+#define LIBSBMLSIM_DOTTED_VERSION "0.9.6"
+#define LIBSBMLSIM_VERSION 906
+#define LIBSBMSIML_VERSION_STRING "00906"
 
 #endif  /* LibSBMLSim_Version_h */
