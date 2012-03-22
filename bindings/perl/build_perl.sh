af5ec4a7de6801948687256ea6edb0fe1c1025d8
Akira Funahashi <funa@bio.keio.ac.jp>
2012-03-22 21:38:06 +0900

    Removed bindings. will be moved under src directory.

--- bindings/perl/build_perl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-swig -perl libsbmlsim.i && \
-/opt/local/bin/perl Makefile.PL && \
-make
-#gcc -fPIC -c libsbmlsim_wrap.c -I`perl -e 'use Config; print $Config{archlib};'`/CORE && \
-#gcc -shared *.o src/CMakeFiles/sbmlsim.dir/*.o -L/usr/local/lib -lsbml -o libsbmlsim.so
