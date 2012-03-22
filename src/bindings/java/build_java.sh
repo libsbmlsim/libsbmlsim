44c0ec52026686aa9e3cbdd8174b8220dd44970d
Akira Funahashi <funa@bio.keio.ac.jp>
2012-03-22 21:43:56 +0900

    Yeaaaahhh!! python binding from SWIG now works!!

---
+++ src/bindings/java/build_java.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+swig -java libsbmlsim.i && \
+gcc -c -Wall -I/System/Library/Frameworks/JavaVM.framework/Headers -I../src libsbmlsim_wrap.c && \
+g++ -shared *.o ../../src/CMakeFiles/sbmlsim.dir/*.o -o libsbmlsim.jnilib -L../../src -L/usr/local/lib -lsbml && \
+mv Test.java Test.java.org && \
+LC_ALL=C javac *.java && \
+rm sbmlsim.jar && \
+LC_ALL=C jar cvf sbmlsim.jar *.class && \
+mv Test.java.org Test.java && \
+javac -cp .:sbmlsim.jar:libsbmlj.jar Test.java && \
+java -cp .:sbmlsim.jar:libsbmlj.jar Test
