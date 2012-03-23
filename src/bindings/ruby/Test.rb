76164f9cf7ce2a55dc96d62f69299d0ea4a4e46f
Akito Tabira <mythosil2008@gmail.com>
2012-03-23 16:54:41 +0900

    forgot to commit new files (ruby related).

---
+++ src/bindings/ruby/Test.rb
@@ -0,0 +1,51 @@
+$:.unshift File.join(File.dirname(__FILE__), ".")
+require 'libsbmlsim'
+
+res = Libsbmlsim::simulateSBMLFromFile('../../MAPK.xml', 4000.0, 0.1, 100, 1, 41, 0)
+
+numOfRows = res.getNumOfRows
+puts "numOfRows: #{numOfRows}"
+
+numOfSp = res.getNumOfSpecies
+puts "numOfSpeies: #{numOfSp}"
+
+numOfParam = res.getNumOfParameters
+puts "numOfParameters: #{numOfParam}"
+
+numOfComp = res.getNumOfCompartments
+puts "numOfCompartments: #{numOfComp}"
+
+timeName = res.getTimeName
+puts "timeName: #{timeName}"
+
+puts "Species Name"
+numOfSp.times do |n|
+  sname = res.getSpeciesNameAtIndex(n)
+  puts "  #{sname}"
+end
+
+puts "Parameter Name"
+numOfParam.times do |n|
+  pname = res.getParameterNameAtIndex(n)
+  puts "  #{pname}"
+end
+
+puts "Compartment Name"
+numOfComp.times do |n|
+  cname = res.getCompartmentNameAtIndex(n)
+  puts "  #{cname}"
+end
+
+puts "Values"
+numOfRows.times do |i|
+  t = res.getTimeValueAtIndex(i)
+  print "#{t}"
+  numOfSp.times do |j|
+    sname = res.getSpeciesNameAtIndex(j)
+    v = res.getSpeciesValueAtIndex(sname, i)
+    print " #{v}"
+  end
+  print "\n"
+  break if i == 10;
+end
+
