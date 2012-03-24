4b557eb3cb1c97cdcda00e646a4bb56058dd027d
Akira Funahashi <funa@bio.keio.ac.jp>
2012-03-25 01:13:00 +0900

    increment version no.

--- src/libsbmlsim/version.h
+++ src/libsbmlsim/version.h
@@ -1,8 +1,8 @@
 #ifndef LibSBMLSim_Version_h
 #define LibSBMLSim_Version_h
 
-#define LIBSBMLSIM_DOTTED_VERSION "0.9.6"
-#define LIBSBMLSIM_VERSION 906
-#define LIBSBMSIML_VERSION_STRING "00906"
+#define LIBSBMLSIM_DOTTED_VERSION "0.9.7"
+#define LIBSBMLSIM_VERSION 907
+#define LIBSBMSIML_VERSION_STRING "00907"
 
 #endif  /* LibSBMLSim_Version_h */
