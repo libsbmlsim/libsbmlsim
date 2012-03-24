41599c5848de015fd8d6df9f6ba9956e956ad977
Akito Tabira <mythosil2008@gmail.com>
2012-03-24 17:24:46 +0900

    added sample code of C++.

---
+++ examples/cpp/test.cpp
@@ -0,0 +1,33 @@
+#include "libsbmlsim/libsbmlsim.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  myResult *rtn;
+
+  double sim_time = 4000;
+  double dt = 0.1;
+  int print_interval = 100;
+  int print_amount = 1;
+  int method = MTHD_RUNGE_KUTTA;
+  boolean use_lazy_method = false;
+
+  if (argc < 2) {
+    printf("Input SBML file is not specified.\n  Usage: %s sbml.xml\n", argv[0]);
+    exit(1);
+  }
+  rtn = simulateSBMLFromFile(argv[1], sim_time, dt, print_interval, print_amount, method, use_lazy_method);
+  if (rtn == NULL) {
+    printf("Returned result is NULL\n");
+  } else if (myResult_isError(rtn)) {
+    cout << "ERROR: " <<  myResult_getErrorMessage(rtn) << endl;
+  } else {
+    /*  print_result(rtn); */
+    write_csv(rtn, "cresult.csv");
+    write_result(rtn, "test.dat");
+  }
+  if (rtn != NULL)
+    free_myResult(rtn);
+  return 0;
+}
