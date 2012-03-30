91187ff584605d05e9a14d442f04aff0899604aa
Akira Funahashi <funa@bio.keio.ac.jp>
2012-03-31 05:09:51 +0900

    Removed unnecessary copy of sample.xml in each bindings. This version will be Version 0.9.8. Must fix perl problem, but will be in next release.

--- src/libsbmlsim/version.h
+++ src/libsbmlsim/version.h
@@ -1,8 +1,8 @@
 #ifndef LibSBMLSim_Version_h
 #define LibSBMLSim_Version_h
 
-#define LIBSBMLSIM_DOTTED_VERSION "0.9.7"
-#define LIBSBMLSIM_VERSION 907
-#define LIBSBMSIML_VERSION_STRING "00907"
+#define LIBSBMLSIM_DOTTED_VERSION "0.9.8"
+#define LIBSBMLSIM_VERSION 908
+#define LIBSBMSIML_VERSION_STRING "00908"
 
 #endif  /* LibSBMLSim_Version_h */
