f95fdb144aac46885a338a48dfc81c2bce07e8a1
Akira Funahashi <funa@bio.keio.ac.jp>
2012-03-31 20:20:04 +0900

    Added some document files. Updated version to 1.0.0!

--- src/libsbmlsim/version.h
+++ src/libsbmlsim/version.h
@@ -1,8 +1,8 @@
 #ifndef LibSBMLSim_Version_h
 #define LibSBMLSim_Version_h
 
-#define LIBSBMLSIM_DOTTED_VERSION "0.9.8"
-#define LIBSBMLSIM_VERSION 908
-#define LIBSBMSIML_VERSION_STRING "00908"
+#define LIBSBMLSIM_DOTTED_VERSION "1.0.0"
+#define LIBSBMLSIM_VERSION 10000
+#define LIBSBMSIML_VERSION_STRING "10000"
 
 #endif  /* LibSBMLSim_Version_h */
