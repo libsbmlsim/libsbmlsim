7b744db1c6669106ab3167b34ce5920fec6d72c3
Akira Funahashi <funa@bio.keio.ac.jp>
2012-04-04 02:26:29 +0900

    little fix to 00remove.sh

--- 00remove.sh
+++ 00remove.sh
@@ -3,9 +3,9 @@
 # ./00remove.sh
 # rm 00remove.sh
 rm -rf **/CVS
-rm -f **/.DS_*
 rm -rf bindings diff simulation_results
 rm -f l?v*
 rm -f src/ev_func_piece.xml
 rm -f src/Makefile.dist
 rm -f header.txt
+rm -f **/.DS_Store
