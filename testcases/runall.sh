e4134639c82525344441624479116fd6f4f334aa
Akira Funahashi <funa@bio.keio.ac.jp>
2013-03-17 02:41:42 +0900

    Modified to skip when SBML file is not found. Support up to 01185.

--- testcases/runall.sh
+++ testcases/runall.sh
@@ -18,32 +18,35 @@ BaseDir="./cases/semantic"
 # concentration:
 
 # Simulate following models with small dt
-fine_delta="00952 00953 00962 00963 00964 00965 00966 00967"
+fine_delta="00952 00953 00962 00963 00964 00965 00966 00967 00986 00987 00988 01000 01113 01121 01123"
 
 # Simulate following models with small atol
-fine_atol="00430 00431 00863 00893 00945 00946 00947 00948"
+fine_atol="00430 00431 00863 00893 00945 00946 00947 00948 01000 01104 01121 01123"
 
 # Simulate following models with small rtol
-fine_rtol="00944 00945 00946 00947 00948"
+fine_rtol="00944 00945 00946 00947 00948 00989 01000 01104 01121 01123"
 
 # Simulate following models with small facmax
-fine_facmax="00426 00430 00431 00863"
+fine_facmax="00426 00430 00431 00863 01000 01104 01121 01123"
 
 # for testcase 00001 .. 00980
-for i in {0000{1..9},000{10..99},00{100..980}}; do
-#for i in 00952 00953; do
-#for i in 00266 00267 00268 00893; do # check for default atol
-#for i in 00430 00431 00863 00893 00945 00946 00947 00948; do # check for fine atol
-#for i in 00952 00962 00964 00965; do # check for fine delta
-
-# for testcase 00001 .. 01123
-#for i in {0000{1..9},000{10..99},00{100..999},0100{0..9},010{10..99},011{00..23}}; do
-#for i in {00{965..999},0100{1..9},010{10..99},011{00..23}}; do
+#for i in {0000{1..9},000{10..99},00{100..980}}; do
 
 # for testcase 00981 .. 01123
 #for i in {00{981..999},0100{0..9},010{10..99},0110{0..9},011{10..23}}; do
 
+# for check
+#for i in 00986 00987 00988 01000 01112 01113 01114 01115 01116 01121; do
+
+# for testcase 00001 .. 01185
+for i in {0000{1..9},000{10..99},00{100..999},0100{0..9},010{10..99},0110{0..9},011{10..85}}; do
   head="$BaseDir/$i/$i"
+  #sbml="$head-sbml-l2v4.xml"
+  sbml="$head-sbml-l3v1.xml"
+  if [[ ! -f "$sbml" ]]; then
+    echo $sbml not found.
+    continue
+  fi
   # 00926 and 00927 contains '\r' before '\n' on each line (DOS format),
   # so we have to call tr -d '\r' before parsing the settings file...
   duration=`tr -d '\r' < "$head-settings.txt" | grep duration | cut -d" " -f 2`
@@ -53,8 +56,6 @@ for i in {0000{1..9},000{10..99},00{100..980}}; do
   concentration=`tr -d '\r' < "$head-settings.txt" | grep amount | cut -d":" -f 2 | sed -e "s/ //g"`
   atol='1e-16'
   rtol='1e-10'
-  #sbml="$head-sbml-l2v4.xml"
-  sbml="$head-sbml-l3v1.xml"
   result="$i-results.csv"
   opt_delta=""
   opt_amount=""
@@ -87,8 +88,8 @@ for i in {0000{1..9},000{10..99},00{100..980}}; do
   fi
   echo "$i: $duration : $steps : [$variables] : [$amount] : [$concentration] : $atol : $rtol"
   echo -en $print_msg
-  #./simulateSBML -t $duration -s $steps $opt_delta -m 1 -n $opt_amount $sbml && \
-  ./simulateSBML -t $duration -s $steps $opt_delta -m 13 -A $atol -R $rtol $opt_facmax -n $opt_amount $sbml && \
+  ./simulateSBML -t $duration -s $steps $opt_delta -m 1 -n $opt_amount $sbml && \
+  #./simulateSBML -t $duration -s $steps $opt_delta -m 13 -A $atol -R $rtol $opt_facmax -n $opt_amount $sbml && \
   ./genresult.pl out.csv $variables $steps > $result 
   echo -en $dbl_sp
   ./compare.pl $i
@@ -111,5 +112,5 @@ done
 unset BaseDir
 unset fine_delta
 rm -f out.csv
-zip result.zip 00*-results.csv
+zip result.zip 0*-results.csv
 #rm 00*-results.csv
