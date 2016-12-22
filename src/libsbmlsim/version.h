29dbcc2a33ebb34c7a534507339e8ed1b7cbc69e
Akira Funahashi <funa@bio.keio.ac.jp>
2016-12-23 02:18:47 +0900

    Updated version number and string

--- src/libsbmlsim/version.h
+++ src/libsbmlsim/version.h
@@ -14,8 +14,8 @@
 #ifndef LibSBMLSim_Version_h
 #define LibSBMLSim_Version_h
 
-#define LIBSBMLSIM_DOTTED_VERSION "1.2.0"
-#define LIBSBMLSIM_VERSION 10200
-#define LIBSBMSIML_VERSION_STRING "10200"
+#define LIBSBMLSIM_DOTTED_VERSION "1.3.0"
+#define LIBSBMLSIM_VERSION 10300
+#define LIBSBMSIML_VERSION_STRING "10300"
 
 #endif  /* LibSBMLSim_Version_h */
