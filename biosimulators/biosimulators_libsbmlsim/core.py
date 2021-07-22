cec653246df8492dcd69cbefc3da432cbd0a1167
Jonathan Karr <jonrkarr@gmail.com>
2021-07-22 10:32:41 -0400

    chore: updating to BioSimulators-utils 0.1.96

--- biosimulators/biosimulators_libsbmlsim/core.py
+++ biosimulators/biosimulators_libsbmlsim/core.py
@@ -9,7 +9,7 @@
 from .data_model import KISAO_ALGORITHMS_MAP, get_integrator
 from biosimulators_utils.combine.exec import exec_sedml_docs_in_archive
 from biosimulators_utils.log.data_model import CombineArchiveLog, TaskLog  # noqa: F401
-from biosimulators_utils.plot.data_model import PlotFormat  # noqa: F401
+from biosimulators_utils.viz.data_model import VizFormat  # noqa: F401
 from biosimulators_utils.report.data_model import ReportFormat, VariableResults  # noqa: F401
 from biosimulators_utils.sedml.data_model import (Task, ModelLanguage, UniformTimeCourseSimulation,  # noqa: F401
                                                   Algorithm, Variable, Symbol)
@@ -45,7 +45,7 @@ def exec_sedml_docs_in_combine_archive(archive_filename, out_dir,
               with reports at keys ``{ relative-path-to-SED-ML-file-within-archive }/{ report.id }`` within the HDF5 file
 
         report_formats (:obj:`list` of :obj:`ReportFormat`, optional): report format (e.g., csv or h5)
-        plot_formats (:obj:`list` of :obj:`PlotFormat`, optional): report format (e.g., pdf)
+        plot_formats (:obj:`list` of :obj:`VizFormat`, optional): report format (e.g., pdf)
         bundle_outputs (:obj:`bool`, optional): if :obj:`True`, bundle outputs into archives for reports and plots
         keep_individual_outputs (:obj:`bool`, optional): if :obj:`True`, keep individual output files
 
